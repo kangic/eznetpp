f5e5fb72eed4d141ecba2da9777ae747e2d3ba41
Kang In Cheol <kangic21@gmail.com>
2015-01-06 23:19:05 +0900

    write to sample code for using library.(It is just for lib concept)

--- test/objtest.cc
+++ test/objtest.cc
@@ -1,9 +1,15 @@
-#include "../src/eznetpp.h"
+#include "../src/runner.h"
 
+// first, I write to example code for making library API concenpt.
+// And then I will write to library codes.
 int main(void) {
-  eznetpp::eznetpp netpp;
+  // it's user class for server
+  eznetpp::usr_server s(ip_addr, port_num, max_accept_nums);
+  eznetpp::runner r;
 
-  netpp.test();
+  r.addEventListener(s);
+  
+  r.run();
   
   return 0;
 }
