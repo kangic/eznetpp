e3954dd0f9d939b57a0d2c1626ef0eed80cf502b
Kang In Cheol <kangic21@gmail.com>
2015-02-23 23:55:52 +0900

    modified the functions' parameters(dummy_connection is a test class)

--- include/if_event_listener.h
+++ include/if_event_listener.h
@@ -1,17 +1,21 @@
 #ifndef _EZNETPP_SRC_IF_EVENT_LISTENER_H_
 #define _EZNETPP_SRC_IF_EVENT_LISTENER_H_
 
+#include <string>
+
 #include "macros.h"
 
 namespace eznetpp {
+class dummy_connection;
 class if_event_listener {
 public:
   virtual ~if_event_listener() {}
   
 public:
-  virtual void on_accept() = 0;
-  virtual void on_read() = 0;
-  virtual void on_write() = 0;
+  virtual void on_accept(const dummy_connection& conn) {}
+  virtual void on_read(const dummy_connection& conn, const std::string& msg
+                     , int len) {}
+  virtual void on_write(const dummy_connection& conn, unsigned int len) {}
 };
   
 }  // namespace eznetpp
