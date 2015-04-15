242bd92245eaffacae3ea120624957a826592857
Kang In Cheol <kangic21@gmail.com>
2015-04-15 23:51:02 +0900

    add while statement for testing echo server

--- test/simple_client.py
+++ test/simple_client.py
@@ -5,7 +5,9 @@ import time
 server_addr = ("localhost", 6666)
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect(server_addr)
-data = client_socket.recv(1024)
-print(data)
-
-time.sleep(30)
+while True:
+    print("test")
+    client_socket.send('msg test')
+    data = client_socket.recv(1024)
+    print(data)
+    time.sleep(1)
