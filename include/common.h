5794e5aa66861fb2fcb4390c4abd807291f7d7d4
Kang In Cheol <kangic21@gmail.com>
2015-04-27 14:54:20 +0900

    add a variable to set max bytes

---
+++ include/common.h
@@ -0,0 +1,11 @@
+// Copyright[2015] <kangic21@gmail.com>
+
+#ifndef INCLUDE_COMMON_H_
+#define INCLUDE_COMMON_H_
+
+namespace eznetpp {
+const int max_transfer_bytes = 4096;
+
+}  // namespace eznetpp
+
+#endif  // INCLUDE_COMMON_H_
