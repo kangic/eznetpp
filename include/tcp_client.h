a589ab53e220730814f90ea6cc8849fc8d6eace8
Kang In Cheol <kangic21@gmail.com>
2015-05-29 21:29:08 +0900

    add some functions

--- include/tcp_client.h
+++ include/tcp_client.h
@@ -4,11 +4,13 @@
 #define INCLUDE_TCP_CLIENT_H_
 
 #include <eznetpp.h>
+#include <if_client.h>
 
 namespace eznetpp {
-class tcp_client {
+class connection;
+class tcp_client : public if_client {
  public:
-  tcp_client();
+  tcp_client() = default;
   virtual ~tcp_client();
 
   // override
@@ -16,7 +18,24 @@ class tcp_client {
 
   int start_async_io();
 
+  void set_server_info(const std::string ip, int port);
+  void set_client_connection(connection* conn);
+
+ protected:
+  void* work_thread(void);
+
  private:
+  int setup_client_socket_and_connect();
+
+  int _client_socket = -1;
+
+  std::string _server_ip = "127.0.0.1";
+  int _server_port = 6666;
+
+  std::thread _work_th;
+
+  connection* _connection = nullptr;
+  
   DISALLOW_COPY_AND_ASSIGN(tcp_client);
 };
 }  // namespace eznetpp
--- src/tcp_client.cc
+++ src/tcp_client.cc
@@ -1,24 +1,115 @@
 //  Copyright [2015] <kangic21@gmail.com>
 
-#include "../include/tcp_client.h"
+#include <tcp_client.h>
 
-
-#include "../include/eznetpp.h"
-
-#include "../include/tcp_connection.h"
+#include <eznetpp.h>
+#include <tcp_connection.h>
 
 namespace eznetpp {
 
-tcp_client::tcp_client(void) {
-
-}
-
 tcp_client::~tcp_client(void) {
-
+  _connection = nullptr;
 }
 
 int tcp_client::start_async_io() {
+  logger::instance().log(logger::log_level::debug
+                         , __FILE__, __FUNCTION__, __LINE__
+                         , "S ->");
+
+  // initialization
+  int ret = setup_client_socket_and_connect();
+  if (ret != 0) {
+    return ret;
+  }
+
+  on_connect(_client_socket);
+
+  _work_th = std::thread(&tcp_client::work_thread, this);
+
+  if (!_work_th.joinable()) {
+    logger::instance().log(logger::log_level::error
+                           , __FILE__, __FUNCTION__, __LINE__
+                           , "failed to create work thread(%d)\n"
+                           , errno);
+    return -1;
+  }
+
+  logger::instance().log(logger::log_level::debug
+                         , __FILE__, __FUNCTION__, __LINE__
+                         , "E <-");
+
   return 0;
+}
+
+int tcp_client::setup_client_socket_and_connect() {
+  _client_socket = socket(AF_INET, SOCK_STREAM, 0);
+
+  if (_client_socket == -1) {
+    return _client_socket;
+  }
+
+  struct sockaddr_in server_addr;
+  bzero(&server_addr, sizeof(server_addr));
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_addr.s_addr = inet_addr(_server_ip.c_str());
+  server_addr.sin_port = htons(_server_port);
+
+  // connect to server(on_connect event)
+  int ret = connect(_client_socket, (struct sockaddr *)&server_addr
+                    , sizeof(server_addr));
+  if (ret == -1) {
+    logger::instance().log(logger::log_level::error
+                           , __FILE__, __FUNCTION__, __LINE__
+                           , "connect error : %d", errno);
+
+    close(_client_socket);
+    _client_socket = -1;
+    return errno;
+  }
+
+  return 0;
+}
+
+void tcp_client::set_server_info(const std::string ip, int port) {
+  _server_ip = ip;
+  _server_port = port;
+}
+
+void tcp_client::set_client_connection(connection* conn) {
+  _connection = conn;
+}
+
+// work thread
+void* tcp_client::work_thread(void) {
+  logger::instance().log(logger::log_level::debug
+                         , __FILE__, __FUNCTION__, __LINE__
+                         , "start work_thread");
+
+  while (1) {
+    int ret = _connection->read_from_socket();
+
+    if (ret <= 0) {
+      _connection->close_socket();
+      logger::instance().log(logger::log_level::debug
+                             , __FILE__, __FUNCTION__, __LINE__
+                             , "Close fd %d"
+                             , _connection->socket_id());
+
+      if (_connection != nullptr) {
+        delete _connection;
+        _connection = nullptr;
+      }
+    } else {
+      logger::instance().log(logger::log_level::debug
+                             , __FILE__, __FUNCTION__, __LINE__
+                             , "read %d bytes"
+                             , ret);
+    }
+
+    usleep(10);
+  }
+
+  return nullptr;
 }
 
 }  // namespace eznetpp
