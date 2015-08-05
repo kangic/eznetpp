5fe81b077a9510e220eff2f25301fde963b8c8cb
Kang In Cheol <kangic21@gmail.com>
2015-08-06 01:30:22 +0900

    update

--- src/sys/io_manager.cc
+++ src/sys/io_manager.cc
@@ -13,17 +13,21 @@ io_manager::~io_manager(void) {
 
 }
 
+int io_manager::init(int max_fds_cnt) {
+  return create_epoll_fd_and_events(max_fds_cnt);
+}
+
 int io_manager::add_fd(int fd) {
   struct epoll_event ev;
 
   ev.events = EPOLLIN;
-  ev.data.fd = cfd;
+  ev.data.fd = fd;
 
-  return epoll_ctl(efd, EPOLL_CTL_ADD, cfd, &ev);
+  return epoll_ctl(_epoll_fd, EPOLL_CTL_ADD, fd, &ev);
 }
 
 int io_manager::del_fd(int fd) {
-  return epoll_ctl(efd, EPOLL_CTL_DEL, cfd, NULL);
+  return epoll_ctl(_epoll_fd, EPOLL_CTL_DEL, fd, NULL);
 }
 
 int io_manager::send_data(int fd, const std::string& data, int len) {
@@ -31,12 +35,7 @@ int io_manager::send_data(int fd, const std::string& data, int len) {
 }
 
 int io_manager::loop(void) {
-  ret = create_epoll_fd_and_events();
-  if (ret != 0) {
-    return ret;
-  }
-
-  _read_th = std::thread(&io_manager::read_thread, this);
+  _read_th = std::thread(&io_manager::read_loop, this);
 
   if (!_read_th.joinable()) {
     logger::instance().log(logger::log_level::error
@@ -57,31 +56,35 @@ void* io_manager::read_loop(void) {
 
 ////////////////////////////////////////////////////////////////////////////////
 // create epoll file descriptor and events
-int io_manager::create_epoll_fd_and_events() {
+int io_manager::create_epoll_fd_and_events(int max_fds_cnt) {
   // create epoll fd and event structures
-  _epoll_fd = epoll_create(_max_connections_cnt);
+  _epoll_fd = epoll_create(max_fds_cnt);
   if (_epoll_fd == -1) {
     logger::instance().log(logger::log_level::error
-                           , __FILE__, __FUNCTION__, __LINE__
-                           , "epoll_create error(%d)"
-                           , errno);
+                          , __FILE__, __FUNCTION__, __LINE__
+                          , "epoll_create error(%d)"
+                          , errno);
   
     return errno;
   }
 
-  _events = new epoll_event[_max_connections_cnt];
+  _events = new epoll_event[max_fds_cnt];
   if (_events == nullptr) {
     logger::instance().log(logger::log_level::error
-                           , __FILE__, __FUNCTION__, __LINE__
-                           , "failed to create events(%d)"
-                           , errno);
+                          , __FILE__, __FUNCTION__, __LINE__
+                          , "failed to create events(%d)"
+                          , errno);
 
     return errno;
   }
 
+  logger::instance().log(logger::log_level::debug
+                        , __FILE__, __FUNCTION__, __LINE__
+                        , "created epoll_fd : %d"
+                        , _epoll_fd);
+
   return 0;
 }
-
  
 }  // namespace sys
 }  // namespace eznetpp
--- src/sys/io_manager.h
+++ src/sys/io_manager.h
@@ -7,47 +7,52 @@
 
 namespace eznetpp {
 namespace sys {
-class socket;
 class io_manager {
  public:
-  static io_manager& instance() {
-    if (_inst == nullptr)
-      _inst = new io_manager();
+  io_manager(void);
+  virtual ~io_manager(void);
 
-    return *_inst;
-  }
+  /*
+   * After the class declaration, must be called this function for preparing 
+   * to work epoll* functions.
+   */
+  int init(int max_fds_cnt = 1000); // TODO(kangic) : define a value
 
+  /*
+   * Add(delete) the descriptor to observe by epoll descriptor.
+   */
   int add_fd(int fd);
   int del_fd(int fd);
 
+  /*
+   * Send the data through the fd.
+   */
   int send_data(int fd, const std::string& data, int len);
 
-  // create read_loop thread in the function.
+  /*
+   * Create the read_loop thread in this function.
+   */
   int loop(void);
 
  protected:
+  /*
+   * 
+   */
   void* read_loop(void);
  
  private:
-   io_manager(void);
-  virtual ~io_manager(void);
+  int create_epoll_fd_and_events(int _max_fds_cnt);
 
-  static io_manager* _inst = null;
-
-  int create_epoll_fd_and_events();
-
+  // variables
   int _epoll_fd = -1;
   struct epoll_event* _events = nullptr;
   int _received_event_fd = -1;
-  int _max_connections_cnt = 1000;
 
   std::thread _read_th;
-
-  std::vector<int> _polling_list;
 
   DISALLOW_COPY_AND_ASSIGN(io_manager);
 };
 }  // namespace sys
 }  // namespace eznetpp
 
-#endif INCLUDE_IO_MANAGER_H_
+#endif  // INCLUDE_IO_MANAGER_H_
