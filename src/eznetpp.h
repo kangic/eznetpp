f50c7965b4524da92e659ae2778670ff340e8db2
Kang In Cheol <kangic21@gmail.com>
2015-08-06 17:56:14 +0900

    update to organize to header files

--- src/eznetpp.h
+++ src/eznetpp.h
@@ -5,27 +5,27 @@
 
 // c headers
 #include <errno.h>
+#include <fcntl.h>
+#include <string.h>
 #include <unistd.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <netinet/tcp.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <sys/epoll.h>
-#include <netinet/in.h>
-#include <netinet/tcp.h>
-#include <arpa/inet.h>
-#include <fcntl.h>
-#include <string.h>
 
 // c++ headers
 #include <algorithm>
 #include <atomic>
-#include <string>
-#include <memory>
-#include <vector>
-#include <map>
-#include <thread>
-#include <mutex>
-#include <condition_variable>
 #include <chrono>
+#include <condition_variable>
+#include <map>
+#include <memory>
+#include <mutex>
+#include <string>
+#include <thread>
+#include <vector>
 
 // eznetpp headers
 #include "common.h"
