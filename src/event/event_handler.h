83542f6ec37c72e67e20baa714a0ee47afc0376c
Kang In Cheol <kangic21@gmail.com>
2015-08-22 17:06:36 +0900

    add the handler

--- src/event/event_handler.h
+++ src/event/event_handler.h
@@ -3,31 +3,50 @@
 #ifndef INCLUDE_EVENT_HANDLER_H_
 #define INCLUDE_EVENT_HANDLER_H_
 
-#include <eznetpp.h>
+#include "eznetpp.h"
 
 namespace eznetpp {
 namespace event {
-class eznetpp::net::connection;
-class event;
+
 class event_handler {
  public:
-  event_handler(void);
-  virtual ~event_handler(void);
+  event_handler(void) = default;
+  virtual ~event_handler(void) = default;
+
+  enum event_handler_type {
+    acceptor = 0,
+    connector,
+  };
+  event_handler_type handler_type() { return _handler_type; };
+
+ protected:
+  event_handler_type _handler_type;
+};
+
+class acceptor_event_handler : public event_handler {
+ public:
+  acceptor_event_handler(void) {
+    _handler_type = event_handler_type::acceptor;
+  };
+  virtual ~acceptor_event_handler(void) = default;
 
   virtual void on_accept(int fd, int err_no) = 0;
+  virtual void on_close(int err_no) = 0;
+};
 
+class connector_event_handler : public event_handler {
+ public:
+  connector_event_handler(void) { 
+    _handler_type = event_handler_type::connector;
+  };
+  virtual ~connector_event_handler(void) = default;
+
+  virtual void on_connect(int err_no) = 0;
   virtual void on_recv(const std::string& msg, int len, int err_no) = 0;
   virtual void on_send(unsigned int len, int err_no) = 0;
   virtual void on_close(int err_no) = 0;
-
- protected:
-  void set_flag(const event_type type);
-
- private:
-  event_type _type;
-
-  DISALLOW_COPY_AND_ASSIGN(event_handler);
 };
+
 }  // namespace event
 }  // namespace eznetpp
-#endif INCLUDE_EVENT_HANDLER_H_
+#endif  // INCLUDE_EVENT_HANDLER_H_
