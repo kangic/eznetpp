bf755cd2515ae6358edb6520c6ba1884b7909863
Kang In Cheol <kangic21@gmail.com>
2015-08-28 15:59:30 +0900

    delete the file

--- src/macros.h
+++ src/macros.h
@@ -3,8 +3,4 @@
 #ifndef INCLUDE_MACROS_H_
 #define INCLUDE_MACROS_H_
 
-#define DISALLOW_COPY_AND_ASSIGN(TypeName) \
-  TypeName(const TypeName&); \
-  void operator=(const TypeName&)
-
 #endif  // INCLUDE_MACROS_H_
