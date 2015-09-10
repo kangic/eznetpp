ab704f7184a167559d589796117deb042f19ef9b
Kang In Cheol <kangic21@gmail.com>
2015-09-10 23:13:01 +0900

    update

--- src/eznetpp.h
+++ src/eznetpp.h
@@ -91,50 +91,6 @@ namespace opt {
 }  // namespace opt
 }  // namespace eznetpp
 
-     /*
-    success = 0,
-
-    sys_eagain = EAGAIN,
-    sys_ewouldblock = EWOULDBLOCK,
-    sys_ebadf = EBADF,
-    sys_ebusy = EBUSY,
-    sys_edeadlk = EDEADLK,
-    sys_eperm = EPERM,
-    sys_edestaddrreq = EDESTADDRREQ,
-    sys_edquot = EDQUOT,
-    sys_efault = EFAULT,
-    sys_efbig = EFBIG,
-    sys_eintr = EINTR,
-    sys_einval = EINVAL,
-    sys_eio = EIO,
-    sys_eisdir = EISDIR,
-    sys_enospc = ENOSPC,
-    sys_epipe = EPIPE,
-    sys_enomem = ENOMEM,
-    sys_emfile = EMFILE,
-    sys_enolck = ENOLCK,
-    sys_enotdir = ENOTDIR,
-    sys_edom = EDOM,
-    sys_eisconn = EISCONN,
-    sys_enoprotoopt = ENOPROTOOPT,
-    sys_enotsock = ENOTSOCK,
-    sys_enobufs = ENOBUFS,
-    sys_econnaborted = ECONNABORTED,
-    sys_eopnotsupp = EOPNOTSUPP,
-    sys_eproto = EPROTO,
-    sys_eacces = EACCES,
-    sys_eaddrinuse = EADDRINUSE,
-    sys_eafnosupport = EAFNOSUPPORT,
-    sys_eaddrnotavail = EADDRNOTAVAIL,
-    sys_ealready = EALREADY,
-    sys_einprogress = EINPROGRESS,
-    sys_etimedout = ETIMEDOUT,
-    sys_eloop = ELOOP,
-    sys_enametoolong = ENAMETOOLONG,
-    sys_enoent = ENOENT,
-    sys_erofs = EROFS,
-    */
-
 // macros
 #define DISALLOW_COPY_AND_ASSIGN(TypeName) \
   TypeName(const TypeName&); \
